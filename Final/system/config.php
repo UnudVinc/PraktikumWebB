f05322e86d5da98471707fd0d9b13b08539dac21
Vincentius Chandra <vincentius.chandra@student.unud.ac.id>
2020-05-22 21:46:18 +0800

    Initial Commit

---
+++ Final/system/config.php
@@ -0,0 +1,13 @@
+<?php
+// Server Database Sources
+$host = "localhost";
+
+// User Database Sources
+$user = "root";
+
+// Password Database Sources
+$pass = "";
+
+// Database for system
+$db = "";
+?>
